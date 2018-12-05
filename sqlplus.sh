1c62659aedf24d1797d97f08eb8a3ee7d95f64e2
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-12-05 22:03:52 +0900

    client version up

--- sqlplus.sh
+++ sqlplus.sh
@@ -1,5 +1,5 @@
-LD_LIBRARY_PATH=/home/application/instantclient_12_1
-PATH=/home/application/instantclient_12_1:$PATH
+LD_LIBRARY_PATH=/home/application/instantclient_18_3
+PATH=/home/application/instantclient_18_3:$PATH
 NLS_LANG=JAPANESE_JAPAN.AL32UTF8
 export LD_LIBRARY_PATH PATH NLS_LANG
 export no_proxy=localhost,192.168.30.4

a31013ee33f5c95193a69b36b321609df8fecd37
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-12-11 13:28:52 +0900

    no_proxyをコメントアウト、login.sqlへのフルパス

--- sqlplus.sh
+++ sqlplus.sh
@@ -1,12 +1,17 @@
+#!/bin/sh
+
+# 各自の環境に合わせて記述
 LD_LIBRARY_PATH=/home/application/instantclient_18_3
 PATH=/home/application/instantclient_18_3:$PATH
+
+# 以降は共通
 NLS_LANG=JAPANESE_JAPAN.AL32UTF8
 export LD_LIBRARY_PATH PATH NLS_LANG
-export no_proxy=localhost,192.168.30.4
+#export no_proxy=localhost,192.168.30.4
 echo -n "ユーザ名："
 read user
 stty -echo
 echo -n "パスワード："
 read pass
 stty echo
-rlwrap sqlplus $user/$pass@172.16.40.4:1521/orcl
+rlwrap sqlplus $user/$pass@172.16.40.4:1521/orcl @/home/application/repo/oracle.setting/login.sql

040d56c0168a265db0f5b00930b34c471d019083
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-12-09 18:16:36 +0900

    instantclientのバージョンアップ

--- sqlplus.sh
+++ sqlplus.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # 各自の環境に合わせて記述
-LD_LIBRARY_PATH=${HOME}/instantclient_18_3
-PATH=${HOME}/instantclient_18_3:$PATH
+LD_LIBRARY_PATH=${HOME}/instantclient_19_5
+PATH=${HOME}/instantclient_19_5:$PATH
 
 # 以降は共通
 NLS_LANG=JAPANESE_JAPAN.AL32UTF8
@@ -14,6 +14,5 @@ stty -echo
 echo -n "パスワード："
 read pass
 stty echo
-#rlwrap sqlplus $user/$pass@192.168.30.3:1521/orcl @/home/application/repo/oracle.setting/login.sql
 #rlwrap sqlplus $user/$pass@172.16.40.4:1521/orcl @/home/application/repo/oracle.setting/login.sql
 rlwrap sqlplus $user/$pass@192.168.30.3:1521/plug19
